b2a2d24ca329b1b476add22c96591386ff744d7d
DominusCarnufex <DominusCarnufex@users.noreply.github.com>
2017-06-21 23:38:25 +0200

    Update and rename install_qemu.sh to install_qemu_and_gcc.sh

--- ci/install_qemu_and_gcc.sh
+++ ci/install_qemu_and_gcc.sh
@@ -2,7 +2,8 @@
 
 # Installs a recent QEMU for the given arch.
 
-sudo apt-get install -y --no-install-recommends curl xz-utils
+sudo apt-get install -y --no-install-recommends curl xz-utils \
+  gcc-$GCC_VARIANT libc6-dev-$ARCH-cross
 curl http://download.qemu.org/qemu-2.9.0.tar.xz | tar xJf -
 cd qemu-2.9.0
 ./configure --target-list=$1-softmmu --disable-docs
