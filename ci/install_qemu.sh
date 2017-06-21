809bdf2d7dd774b24f8723ddb657769444fbaaf9
DominusCarnufex <DominusCarnufex@users.noreply.github.com>
2017-06-21 18:39:16 +0200

    Update install_qemu.sh

--- ci/install_qemu.sh
+++ ci/install_qemu.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y --no-install-recommends curl xz-utils
 curl http://download.qemu.org/qemu-2.9.0.tar.xz | tar xJf -
 cd qemu-2.9.0
 ./configure --target-list=$1-softmmu --disable-docs
-time make -j4
-sudo make install
+make -s -j4
+sudo make -s install
 cd ..
