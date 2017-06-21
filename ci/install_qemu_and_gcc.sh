0204ac24dfd31ebe3a19b68e743c4b3e0e345b2f
DominusCarnufex <DominusCarnufex@users.noreply.github.com>
2017-06-21 23:43:42 +0200

    Update install_qemu_and_gcc.sh

--- ci/install_qemu_and_gcc.sh
+++ ci/install_qemu_and_gcc.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-# Installs a recent QEMU for the given arch.
+# Installs a recent QEMU and a cross compiler for the given arch.
+
+if [ -z $LIBC_VARIANT ]; then LIBC_VARIANT=$ARCH; fi
 
 sudo apt-get install -y --no-install-recommends curl xz-utils \
-  gcc-$GCC_VARIANT libc6-dev-$ARCH-cross
+  gcc-$GCC_VARIANT libc6-dev-$LIBC_VARIANT-cross
 curl http://download.qemu.org/qemu-2.9.0.tar.xz | tar xJf -
 cd qemu-2.9.0
-./configure --target-list=$1-softmmu --disable-docs
+./configure --target-list=$ARCH-softmmu --disable-docs
 make -s -j4
 sudo make -s install
 cd ..
