c1a20bdfbd54f616a3df7bdceb762cccadc1c0a1
DominusCarnufex <DominusCarnufex@users.noreply.github.com>
2017-07-06 20:59:11 +0200

    Update install_qemu_and_gcc.sh

--- ci/install_qemu_and_gcc.sh
+++ ci/install_qemu_and_gcc.sh
@@ -8,6 +8,9 @@ sudo apt-get install -y --no-install-recommends curl xz-utils \
   gcc-$GCC_VARIANT libc6-dev-$LIBC_VARIANT-cross
 curl http://download.qemu.org/qemu-2.9.0.tar.xz | tar xJf -
 cd qemu-2.9.0
+# Custom built kernel won’t boot on PowerPC if this patch isn’t applied.
+#if [ "$ARCH" == "ppc" ]; then patch -p0 < ../ci/ppc_qemu.patch; fi
+patch -p0 < ../ci/ppc_qemu.patch
 ./configure --target-list=$ARCH-softmmu --disable-docs
 make -s -j4
 sudo make -s install
